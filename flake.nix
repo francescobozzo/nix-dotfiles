8c5b53d2308d5114a093f6bae3863720154962b8
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2025-04-25 14:19:32 +0200

    Add setup for finder

--- flake.nix
+++ flake.nix
@@ -34,6 +34,8 @@
       system.defaults = {
         dock = {
           autohide = true;
+          autohide-delay = 0.24;
+          autohide-time-modifier = 0.9;
           show-recents = false;
           persistent-apps = [
             "/Applications/WhatsApp.app"
@@ -43,6 +45,15 @@
             "/Applications/Xcode.app"
             "/Users/fbozzo/Applications/Home Manager Apps/Visual Studio Code.app"
           ];
+        };
+        finder = {
+          AppleShowAllFiles = true;
+          ShowStatusBar = true;
+          ShowPathbar = true;
+          FXRemoveOldTrashItems = true;  # Remove items in the trash after 30 days
+          FXPreferredViewStyle = "Nlsv"; # List view
+          AppleShowAllExtensions = true;
+          _FXShowPosixPathInTitle = true;
         };
       };
 
