9993313b7ec240910e29a4e1a8570a3652de5823
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2025-08-12 23:59:25 +0200

    Add git aliases

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -5,6 +5,11 @@
     userEmail = "francesco.bozzo.99@gmail.com";
     ignores = [ ".DS_Store" ];
 
+    aliases = {
+      dag = "log --graph --format='format:%C(yellow)%h%C(reset) %C(blue)%an <%ae>%C(reset) %C(magenta)%cr%C(reset)%C(auto)%d%C(reset)%n%s' --date-order";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+    };
+
     extraConfig = {
       user.signingkey = "~/.ssh/id_ed25519.pub";
 
