d39cfbaf0f62c963c3f097cac2dea42f8d2f2912
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2025-12-15 02:12:25 +0100

    Add authorized key to user ssh login

--- hosts/framework-desktop/users.nix
+++ hosts/framework-desktop/users.nix
@@ -12,11 +12,16 @@
       ];
       shell = pkgs.zsh;
       hashedPassword = "$6$w6UYvw/qfQjnZn4s$X4JoDYFZZmCmMdq4LUcxflRhK98KuVs3DerIA4aaUb1pQVE9jxKAnC8ptIjgc7oKU1Qg0xRGkz2/V1QFg.zb9/";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNF1yDeweP0dS1VdongvQqcnwfqnRm/iV9rr0EVk2t/ francesco.bozzo.99@gmail.com"
+      ];
     };
 
-    root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNF1yDeweP0dS1VdongvQqcnwfqnRm/iV9rr0EVk2t/ francesco.bozzo.99@gmail.com"
-    ];
-    root.hashedPassword = "$6$w6UYvw/qfQjnZn4s$X4JoDYFZZmCmMdq4LUcxflRhK98KuVs3DerIA4aaUb1pQVE9jxKAnC8ptIjgc7oKU1Qg0xRGkz2/V1QFg.zb9/";
+    root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNF1yDeweP0dS1VdongvQqcnwfqnRm/iV9rr0EVk2t/ francesco.bozzo.99@gmail.com"
+      ];
+      hashedPassword = "$6$w6UYvw/qfQjnZn4s$X4JoDYFZZmCmMdq4LUcxflRhK98KuVs3DerIA4aaUb1pQVE9jxKAnC8ptIjgc7oKU1Qg0xRGkz2/V1QFg.zb9/";
+    };
   };
 }
