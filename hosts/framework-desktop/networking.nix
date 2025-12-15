ab25f41948701a8b3746508211dae14c1bea27ff
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2025-12-15 01:52:52 +0100

    Add fail2ban

--- hosts/framework-desktop/networking.nix
+++ hosts/framework-desktop/networking.nix
@@ -20,4 +20,22 @@
   # networking.interfaces.wlp192s0.useDHCP = lib.mkDefault true;
 
   services.openssh.enable = true;
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    ignoreIP = [
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime-increment = {
+      enable = true; # Enable increment of bantime after each violation
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # Do not ban for more than 1 week
+      overalljails = true; # Calculate the bantime based on all the violations
+    };
+    jails = { }; # TODO: add reverse proxy
+  };
 }
