22c14619795ddd419d80b96c5fd75edf9e2229df
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2026-01-06 03:56:04 +0100

    Add timestamp suffix in logrotate

--- hosts/framework-desktop/services/logrotate.nix
+++ hosts/framework-desktop/services/logrotate.nix
@@ -1,12 +1,18 @@
 { config, ... }:
 {
+  # TODO: compress logs
+  # Example: https://github.com/khaneliman/khanelinix/blob/9a9a3318d552e6f89c56f8f7ed8113ae01028681/modules/nixos/services/logrotate/default.nix#L22
   services.logrotate = {
     enable = true;
     settings = {
+      header = {
+        dateext = true;
+        dateformat = "-%Y-%m-%d";
+        missingok = true;
+      };
       "${config.services.traefik.staticConfigOptions.accessLog.filePath}" = {
         frequency = "daily";
         rotate = 7;
-        missingok = true;
         notifempty = true;
         postrotate = ''
           systemctl kill --signal=USR1 traefik
