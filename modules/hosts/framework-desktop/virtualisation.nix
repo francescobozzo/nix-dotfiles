22bf51fd6930f1fc9d8191fde502b1411fdc9e9c
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2026-08-01 17:06:47 +0200

    Bump docker version to fix act error

--- modules/hosts/framework-desktop/virtualisation.nix
+++ modules/hosts/framework-desktop/virtualisation.nix
@@ -1,11 +1,12 @@
 {
-  flake.modules.nixos.fd = {
+  flake.modules.nixos.fd = { pkgs, lib, ... }: {
     virtualisation = {
       oci-containers.backend = "docker";
       podman.enable = false;
       docker = {
         enable = true;
         enableOnBoot = true;
+        package = pkgs.unstable.docker; # https://github.com/nektos/act/issues/6092
         autoPrune = {
           enable = true;
           flags = [ ];
